27df1177525a1f64c1821951faff034a9652c335
Vu Tran <minh-vu.tran@alten.nl>
2023-06-22 00:10:32 +0200

    Change writer to reader

--- result/plot_timeline.py
+++ result/plot_timeline.py
@@ -29,14 +29,14 @@ subscriber = pu.process_dataframe(df, 'Subscriber', subscriber_map, start_time, 
 timer = pu.process_dataframe(df, 'Timer', timer_map, start_time, frame_id=True)
 executor = pu.process_dataframe(df, 'Executor', executor_map, start_time, frame_id=False)
 publisher = pu.process_dataframe(df, 'Publisher', publisher_map, start_time, frame_id=False)
-listener = pu.process_dataframe(df, 'Listener', executor_map, start_time, frame_id=False)
-reader = pu.process_dataframe(df, 'Reader', executor_map, start_time, frame_id=False)
+listener = pu.process_dataframe(df, 'Listener', executor_map, start_time, frame_id=True)
+writer = pu.process_dataframe(df, 'Writer', executor_map, start_time, frame_id=True)
 
 # Get 20 random consecutive rows from sub dataframe
 #start_index = timer.sample(n=1).index[0]
-start_index = 24
+start_index = 6
 # get the 3 consecutive rows starting from the random start index
-filtered_timer = timer.iloc[start_index:start_index+6]
+filtered_timer = timer.iloc[start_index:start_index+12]
 
 # Find min and max time from these random rows
 min_time = np.min(filtered_timer['Time'])
@@ -49,7 +49,7 @@ filtered_subscriber = pu.get_filtered_times(subscriber, x_min, max_time)
 filtered_executor = pu.get_filtered_times(executor, x_min, max_time)
 filtered_publisher = pu.get_filtered_times(publisher, x_min, max_time)
 filtered_listener = pu.get_filtered_times(listener, x_min, max_time)
-filtered_reader = pu.get_filtered_times(reader, x_min, max_time)
+filtered_writer = pu.get_filtered_times(writer, x_min, max_time)
 
 executor1 = ['Timer1', 'Executor1', 'Publisher1']
 executor2 = ['Subscriber1', 'Executor2', 'Publisher2']
@@ -72,33 +72,33 @@ filtered_listener2 = filtered_listener[filtered_listener['ExecutorID'].isin(exec
 filtered_listener3 = filtered_listener[filtered_listener['ExecutorID'].isin(executor3)]
 filtered_listener4 = filtered_listener[filtered_listener['ExecutorID'].isin(executor4)]
 
-filtered_reader2 = filtered_reader[filtered_reader['ExecutorID'].isin(executor2)]
-filtered_reader3 = filtered_reader[filtered_reader['ExecutorID'].isin(executor3)]
-filtered_reader4 = filtered_reader[filtered_reader['ExecutorID'].isin(executor4)]
+filtered_writer2 = filtered_writer[filtered_writer['ExecutorID'].isin(executor2)]
+filtered_writer3 = filtered_writer[filtered_writer['ExecutorID'].isin(executor3)]
+filtered_writer4 = filtered_writer[filtered_writer['ExecutorID'].isin(executor4)]
 
 fig, axs = plt.subplots(4, sharex=False)
 pu.plot_filtered_data(axs[0], filtered_publisher1, 'Publisher1', ':', 'black')
 pu.plot_filtered_data(axs[0], filtered_timer, 'Timer1', '--', 'cyan', frame_id=True)
-#pu.plot_filtered_data(axs[0], filtered_executor1, 'Executor1', ':', 'red')
+pu.plot_filtered_data(axs[0], filtered_executor1, 'Executor1', ':', 'red')
 axs[0].set_title('Executor 1')
 
 pu.plot_filtered_data(axs[1], filtered_listener2, 'Executor2', '-.', 'green')
-pu.plot_filtered_data(axs[1], filtered_reader2, 'Executor2', '-.', 'black')
+pu.plot_filtered_data(axs[1], filtered_writer2, 'Executor2', '-.', 'black')
 pu.plot_filtered_data(axs[1], filtered_publisher2, 'Publisher2', ':', 'black')
 pu.plot_filtered_data(axs[1], filtered_subscriber2, 'Subscriber1', '--', 'cyan', frame_id=True)
-#pu.plot_filtered_data(axs[1], filtered_executor2, 'Executor2', ':', 'red')
+pu.plot_filtered_data(axs[1], filtered_executor2, 'Executor2', ':', 'red')
 axs[1].set_title('Executor 2')
 
 pu.plot_filtered_data(axs[2], filtered_listener3, 'Executor3', '-.', 'green')
-pu.plot_filtered_data(axs[2], filtered_reader3, 'Executor3', '-.', 'black')
+pu.plot_filtered_data(axs[2], filtered_writer3, 'Executor3', '-.', 'black')
 pu.plot_filtered_data(axs[2], filtered_publisher3, 'Publisher3', ':', 'black')
 pu.plot_filtered_data(axs[2], filtered_subscriber3, 'Subscriber2', '--', 'cyan', frame_id=True)
-#pu.plot_filtered_data(axs[2], filtered_executor3, 'Executor3', ':', 'red')
+pu.plot_filtered_data(axs[2], filtered_executor3, 'Executor3', ':', 'red')
 axs[2].set_title('Executor 3')
 
 pu.plot_filtered_data(axs[3], filtered_listener4, 'Executor4', '-.', 'green')
 pu.plot_filtered_data(axs[3], filtered_subscriber4, 'Subscriber3', '--', 'cyan', frame_id=True)
-#pu.plot_filtered_data(axs[3], filtered_executor4, 'Executor4', ':', 'red')
+pu.plot_filtered_data(axs[3], filtered_executor4, 'Executor4', ':', 'red')
 axs[3].set_title('Executor 4')
 
 print(f"Start Program at: {start_program_time:.2f}")
